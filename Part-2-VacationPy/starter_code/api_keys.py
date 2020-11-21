d582c4d17ce6bfd421d84c84f9fc11b2f09167d3
Brooke Cooper <techanalyst65@gmail.com>
2020-11-21 03:51:07 -0500

    Now uploading completed VacationPy APIs optional homework assignment

--- Part-2-VacationPy/starter_code/api_keys.py
+++ Part-2-VacationPy/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "7ec08379d4519e16b3a72d919890d445"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDEpco07YJ1Y0m1W0ALX9mLUjCumVN9j5Y"
