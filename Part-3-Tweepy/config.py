bf912660477fc125c0f0d264890c8edaa9e43c79
Brooke Cooper <techanalyst65@gmail.com>
2020-11-23 05:02:29 -0500

    Uploading optional python APIs homework with fully completed WeatherPy and VacationPy assignments and partially completed Tweepy assignment.

---
+++ Part-3-Tweepy/config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = '3Hpt3BSz6t20BqcZrygLwtOZ9'
+consumer_secret = 'rX9ofDM2IBFE4Qw5BH1rnlw0H1KCnOUxurhv6gZD8x7fS0vPsU'
+access_token = '1326308341879087104-HTggZeFsT2mN16anaqIEWJO9QYRPwe'
+access_token_secret = 'r8ik1MxGw378k5Z94pHQ2PLGcUWI1ARDJutsGt19DBTIM'
